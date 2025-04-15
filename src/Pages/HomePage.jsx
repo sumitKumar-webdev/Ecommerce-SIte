7e82839ccc46b8243a8ce2baf2322d827aefe09b
Sumit Kumar <sumitkr8178@gmail.com>
2025-04-15 10:18:11 +0530

    add .jsx in imgSlider

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ImgSlider } from '../Component/ImgSlider'
+import { ImgSlider } from '../Component/ImgSlider.jsx'
 import { CategorySection } from '../Component/CategorySection'
 
 /**
