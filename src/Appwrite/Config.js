5c5efbe8f5e471a28970fe60ce42e302d957b3d2
Sumit Kumar <sumitkr8178@gmail.com>
2025-04-27 15:13:57 +0530

    changed file view Service

--- src/Appwrite/Config.js
+++ src/Appwrite/Config.js
@@ -54,7 +54,7 @@ export class appwriteServices {
 
      getProductImg(fileID){
         try {
-            return  this.bucket.getFilePreview(
+            return  this.bucket.getFileView(
                 Conf.appwriteBucketId,
                 fileID,
             )
